efa65eb8b5c1fe56978c3816346c61ebcddbb7ec
adipatil312 <159871791+adipatil312@users.noreply.github.com>
2024-03-09 21:10:59 +0530

    Create PersonalInformation1.java

---
+++ PersonalInformation1.java
@@ -0,0 +1,20 @@
+class PersonalInformation1
+{
+public static void main(String[] args)
+{
+String Name= "Patil Aditya Dhananjay";
+String Address= "Borgaon";
+String City= "Solapur";
+String College="S.B.Patil Indapur";
+String Department= "Computer";
+String Roll= "101";
+String Contact= "8530744581";
+System.out.println(Name);
+System.out.println(Address);
+System.out.println(City);
+System.out.println(College);
+System.out.println(Department);
+System.out.println(Roll);
+System.out.println(Contact);
+}
+}
