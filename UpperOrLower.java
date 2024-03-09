4edf565ba8666deb519efa465dbbd69b9a8090cc
adipatil312 <159871791+adipatil312@users.noreply.github.com>
2024-03-09 21:09:06 +0530

    Create UpperOrLower.java

---
+++ UpperOrLower.java
@@ -0,0 +1,9 @@
+class UpperOrLower
+{
+public static void main(String[] args)
+{
+char value='A';
+String result=(value>=65 && value<=90)?("Given alphabet is in upper case"):("Given alphabet is in not upper case");
+System.out.println(result);
+}
+}
