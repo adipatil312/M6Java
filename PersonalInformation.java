9d825f85bf9f239bdc61b3ec07119f5fd3d91c30
adipatil312 <159871791+adipatil312@users.noreply.github.com>
2024-03-09 21:11:44 +0530

    Create PersonalInformation.java

---
+++ PersonalInformation.java
@@ -0,0 +1,15 @@
+class PersonalInformation
+{
+public static void main(String[] args)
+{
+System.out.println("Name: Patil Aditya Dhananjay");
+System.out.println("Birthdate: 16/05/2002");
+System.out.println("Age: 23");
+System.out.println("Address: Borgaon");
+System.out.println("City: Solapur");
+System.out.println("Contact number: 8530744581");
+System.out.println("College name: S.B.Patil Indapur");
+System.out.println("Department: Computer");
+System.out.println("Roll no: 101");
+}
+}
