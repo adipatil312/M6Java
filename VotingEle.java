0e75a45a49fceea28eb84a2781ad402d75658d4f
adipatil312 <159871791+adipatil312@users.noreply.github.com>
2024-03-09 21:07:18 +0530

    Create VotingEle.java

---
+++ VotingEle.java
@@ -0,0 +1,15 @@
+class VotingEle
+{
+public static void main(String[]args)
+{
+int age=17;
+boolean result=age>=18;
+System.out.println("Your age fot voting is:"+result);
+int age1=21;
+boolean result1=age1<=25;
+System.out.println("Your age for government exam is:"+result1);
+}
+}
+
+/* Output=  Your age fot voting is:false
+   Your age for government exam is:true*/
