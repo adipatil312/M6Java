2d5ab393aa0f68b3c0fe9aea0f9f7d370a0c97d6
adipatil312 <159871791+adipatil312@users.noreply.github.com>
2024-03-10 18:24:27 +0530

    Create GreaterNum.java

---
+++ GreaterNum.java
@@ -0,0 +1,13 @@
+class GreaterNum
+{
+public static void main(String [] args)
+{
+int a=30;
+int b=40;
+int c=50;
+int d=(a>b)?(a):(b);
+int e=(c>d)?(c):(d);
+System.out.println(e);
+}
+}
+//Output=50
