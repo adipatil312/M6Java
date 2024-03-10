1ac15e85c120804cb614635d7ccb01b97b9d22a8
adipatil312 <159871791+adipatil312@users.noreply.github.com>
2024-03-10 18:26:53 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+class EvenOdd
+{
+public static void main(String [] args)
+{
+char c='A';
+String result=(c%2==0)?("the alphabet is even"):("the alphabet is even");
+System.out.println(result);
+
+
+}
+}
