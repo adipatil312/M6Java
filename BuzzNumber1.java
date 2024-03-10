bd6474eb560d6bb39b4864f41d0188b7f5502b2f
adipatil312 <159871791+adipatil312@users.noreply.github.com>
2024-03-10 18:29:10 +0530

    Create BuzzNumber1.java

---
+++ BuzzNumber1.java
@@ -0,0 +1,10 @@
+class BuzzNumber1
+{
+public static void main(String[]args)
+{
+int num=14;
+if(num%7==0||num%10==7)
+{System.out.println("It is a buzz number");}
+else{System.out.println("It is not buzz number");}
+}
+}
