d89fe7764df94c9bfcbec2fbcc29cbb6b6de18ea
adipatil312 <159871791+adipatil312@users.noreply.github.com>
2024-03-10 18:23:45 +0530

    Create IfElse.java

---
+++ IfElse.java
@@ -0,0 +1,11 @@
+class IfElse
+{
+public static void main(String [] args)
+{
+int a=19;
+if(a>=19)
+{System.out.println("Your elligible for voting");}
+else
+{System.out.println("Your not eligible for voting");}
+}
+}
