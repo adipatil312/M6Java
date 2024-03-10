d465bffbf4e795a0ab5be64d47da0b34b4a80d2a
adipatil312 <159871791+adipatil312@users.noreply.github.com>
2024-03-10 18:22:41 +0530

    Update IfElseBuzz.java

--- IfElseBuzz.java
+++ IfElseBuzz.java
@@ -1,1 +1,17 @@
-  
+class IfElseBuzz
+{
+public static void main(String []args)
+{
+int a=107;
+if (a>=0 && a<=99)
+{
+if(a%10==7 || a%2==0)
+{
+System.out.println(a+" is a buzz number");
+}
+else
+{System.out.println(a+" is not buzz number");}}
+else
+{System.out.println(a+" is not two digit number");}
+}
+}  
