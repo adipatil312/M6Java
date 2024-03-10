388920348bfe25dce66db45c2a4bc704126302bd
adipatil312 <159871791+adipatil312@users.noreply.github.com>
2024-03-10 18:30:08 +0530

    Create ArithmeticPlusOp1.java

---
+++ ArithmeticPlusOp1.java
@@ -0,0 +1,14 @@
+class ArithmeticPlusOp1
+{
+public static void main(String[] args)
+{
+String Name= "Patil Aditya Dhananjay";
+String Address= "Borgaon";
+String City= "Solapur";
+String College="S.B.Patil Indapur";
+String Department= "Computer";
+int Roll= 101;
+String Contact= "8530744581";
+System.out.println(Name+Address+City+College+Department+Roll+Contact);
+}
+}
